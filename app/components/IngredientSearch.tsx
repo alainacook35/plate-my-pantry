ce4ad381bad7a9ed5a243f1beac89d9b403b2465
Alaina Cook <alaina.leeann35@gmail.com>
2026-06-11 16:12:43 -0400

    feat: instructions for usage

--- app/components/IngredientSearch.tsx
+++ app/components/IngredientSearch.tsx
@@ -2,6 +2,9 @@
 import {
   Autocomplete,
   Box,
+  Button,
+  Dialog,
+  IconButton,
   SxProps,
   TextField,
   Typography,
@@ -11,6 +14,8 @@ import { Dispatch, SetStateAction, useState } from "react";
 import { IIngredient } from "../utils/interfaces";
 import { AxiosResponse } from "axios";
 import { useToast } from "../hooks/useToast";
+import { FaQuestionCircle } from "react-icons/fa";
+import { FaXmark } from "react-icons/fa6";
 
 export default function IngredientSearch({
   selectedIngredients,
@@ -25,11 +30,11 @@ export default function IngredientSearch({
 
   const [inputValue, setInputValue] = useState("");
   const [results, setResults] = useState<IIngredient[]>([]);
-
   const [open, setOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
   const { toast } = useToast();
-  
+
   const searchIngredients = async (searchTerm: string) => {
     if (searchTerm.trim() !== "") {
       spoonacularInstance
@@ -39,14 +44,14 @@ export default function IngredientSearch({
           setOpen(true);
         })
         .catch((err) => {
-          let errorMsg = ""
+          let errorMsg = "";
           if (err.response.status === 402) {
-            errorMsg = "API quota reached. Please try again later."
+            errorMsg = "API quota reached. Please try again later.";
           } else {
-            errorMsg = "Internal Server Error"
+            errorMsg = "Internal Server Error";
           }
 
-          toast(errorMsg, { severity: 'error'})
+          toast(errorMsg, { severity: "error" });
         });
     }
   };
@@ -63,41 +68,79 @@ export default function IngredientSearch({
   return (
     <Box sx={sx}>
       <Typography variant="body1">Add your ingredients</Typography>
-      <Autocomplete
-        open={open}
-        disableCloseOnSelect={true}
-        clearOnEscape={false}
-        clearOnBlur={false}
-        multiple
-        filterOptions={(opt) => opt}
-        onInputChange={(_, newValue, reason) => {
-          if (reason === "input") {
-            onSearchChange(newValue);
-          }
-        }}
-        onKeyDown={(e) => {
-          if (e.key === "Enter" && !open) {
-            e.preventDefault();
-            searchIngredients(inputValue);
-          }
-        }}
-        renderInput={(params) => (
-          <TextField placeholder="Search for ingredients" {...params} />
-        )}
-        options={results}
-        getOptionLabel={(opt: IIngredient) => opt?.name}
-        noOptionsText="No matching ingredients found"
-        onBlurCapture={() => setOpen(false)}
-        onFocus={() => {
-          if (results.length > 0) {
-            setOpen(true);
-          }
-        }}
-        value={selectedIngredients}
-        onChange={(_, value) => {
-          setSelectedIngredients(value);
-        }}
-      />
+      <Box sx={{ display: "flex", gap: "5px" }}>
+        <Autocomplete
+          slotProps={{
+            paper: { sx: { background: "white" } },
+            root: {
+              sx: { background: "white", borderRadius: "5px", width: "100%" },
+            },
+          }}
+          open={open}
+          disableCloseOnSelect={true}
+          clearOnEscape={false}
+          clearOnBlur={false}
+          multiple
+          filterOptions={(opt) => opt}
+          onInputChange={(_, newValue, reason) => {
+            if (reason === "input") {
+              onSearchChange(newValue);
+            }
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && !open) {
+              e.preventDefault();
+              searchIngredients(inputValue);
+            }
+          }}
+          renderInput={(params) => (
+            <TextField placeholder="Search for ingredients" {...params} />
+          )}
+          options={results}
+          getOptionLabel={(opt: IIngredient) => opt?.name}
+          noOptionsText="No matching ingredients found"
+          onBlurCapture={() => setOpen(false)}
+          onFocus={() => {
+            if (results.length > 0) {
+              setOpen(true);
+            }
+          }}
+          value={selectedIngredients}
+          onChange={(_, value) => {
+            setSelectedIngredients(value);
+          }}
+        />
+        <Box sx={{ display: "flex", margin: "auto" }}>
+          <IconButton color="secondary" onClick={() => setModalOpen(true)}>
+            <FaQuestionCircle />
+          </IconButton>
+        </Box>
+      </Box>
+      <Dialog open={modalOpen} onClose={() => setModalOpen(false)}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "1px",
+            maxWidth: "400px",
+          }}
+        >
+          <Box sx={{ margin: "20px" }}>
+            <Typography variant="body1">
+              {
+                "Plate My Pantry takes ingredients from your pantry (or your refrigerator!) and turns them into delicious recipes."
+              }
+            </Typography>
+            <br />
+            <Typography variant="body1">
+              {
+                "Simply type the name of your ingredient, press enter, and select it from the provided list. Once you've added all your desired ingredients, press the Find a Recipe button to find your next meal."
+              }
+            </Typography>
+              <Box sx={{ marginTop: "20px", display: "flex", width: "100%", justifyContent: "center"}}><Button onClick={() => setModalOpen(false)} size="small" variant="contained" color="secondary">Get Started</Button></Box>
+          </Box>
+        </Box>
+      </Dialog>
     </Box>
   );
 }
